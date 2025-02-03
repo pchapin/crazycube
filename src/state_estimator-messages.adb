93ff1de262119673b6c67cbdbc60500e445fda89
Slick-Nick0127 <nickslocik2003@gmail.com>
2025-02-03 15:28:19 -0500

    updated state-estimater into state-estimator: spelled wrong originally

--- src/state_estimator-messages.adb
+++ src/state_estimator-messages.adb
@@ -1,17 +1,17 @@
 --------------------------------------------------------------------------------
--- FILE   : state_estimater-messages.adb
+-- FILE   : state_estimator-messages.adb
 -- SUBJECT: Body of a package that implements the main part of the module.
 -- AUTHOR : (C) Copyright 2024 by Vermont State University
 --
 --------------------------------------------------------------------------------
 pragma SPARK_Mode(On);
 
-with Message_Manager;    -- See the comments in state_estimater-api.ads.
-with Name_Resolver;      -- See the comments in state_estimater-api.ads.
-with State_Estimater.API;  -- Needed so that the types in the API can be used here.
+with Message_Manager;    -- See the comments in state_estimator-api.ads.
+with Name_Resolver;      -- See the comments in state_estimator-api.ads.
+with State_Estimator.API;  -- Needed so that the types in the API can be used here.
 with CubedOS.Log_Server.API;
 
-package body State_Estimater.Messages is
+package body State_Estimator.Messages is
    use Message_Manager;
 
    procedure Initialize is
@@ -24,11 +24,11 @@ package body State_Estimater.Messages is
    -------------------
 
    procedure Handle_A_Request(Message : in Message_Record)
-     with Pre => State_Estimater.API.Is_A_Request(Message)
+     with Pre => State_Estimator.API.Is_A_Request(Message)
    is
       Status : Message_Status_Type;
    begin
-      State_Estimater.API.A_Request_Decode(Message, Status);
+      State_Estimator.API.A_Request_Decode(Message, Status);
 
    end Handle_A_Request;
 
@@ -38,10 +38,10 @@ package body State_Estimater.Messages is
 
    procedure Process(Message : in Message_Record) is
    begin
-      if State_Estimater.API.Is_A_Request(Message) then
+      if State_Estimator.API.Is_A_Request(Message) then
          Handle_A_Request(Message);
       else
-         CubedOS.Log_Server.API.Log_Message(Name_Resolver.State_Estimater,
+         CubedOS.Log_Server.API.Log_Message(Name_Resolver.State_Estimator,
                                             CubedOS.Log_Server.API.Error,
                                             "An unknown message type has been received!");
       end if;
@@ -57,9 +57,9 @@ package body State_Estimater.Messages is
    begin
       Initialize;
       loop
-         Message_Manager.Fetch_Message(Name_Resolver.State_Estimater.Module_ID, Incoming_Message);
+         Message_Manager.Fetch_Message(Name_Resolver.State_Estimator.Module_ID, Incoming_Message);
          Process(Incoming_Message);
       end loop;
    end Message_Loop;
 
-end State_Estimater.Messages;
+end State_Estimator.Messages;
