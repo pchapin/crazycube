54a10d27a624334188710c6dba33e159dca423e8
Katie Niekrewicz <kxn05280@vsc.edu>
2025-03-03 11:54:37 -0500

    added to motors module

--- src/motors-messages.adb
+++ src/motors-messages.adb
@@ -6,42 +6,36 @@
 --------------------------------------------------------------------------------
 pragma SPARK_Mode(On);
 
-with Message_Manager;    -- See the comments in SAMPLE_MODULE-api.ads.
-with Name_Resolver;      -- See the comments in SAMPLE_MODULE-api.ads.
+with Message_Manager;
+with Name_Resolver;
 with motors.API;  -- Needed so that the types in the API can be used here.
 with CubedOS.Log_Server.API;
 
 package body motors.Messages is
    use Message_Manager;
 
-   -- The package initializer, if needed. This procedure might be called as the message loop
-   -- (see below) is starting, or perhaps during package elaboration. If this procedure is not
-   -- needed, it should be removed to avoid SPARK flow warnings.
-   --
-   procedure Initialize is
-   begin
-      null;
-   end Initialize;
-
    -------------------
    -- Message Handling
    -------------------
 
-   -- Here is where the details of handing the messages is done. Probably there will be a
-   -- separate subprogram for each message, but the exact organization is, obviously, dependent
-   -- on the needs of the module. It might be useful to put these message handling subprograms
-   -- into a private sibling package. That would move the complex details of message handling to
-   -- a different file and reduce clutter in this file. This file is really just about decoding
-   -- and dispatching the messages. We recommend that if a single internal package is used that
-   -- it should be called Sample_Module.Core (for example).
-
-   procedure Handle_A_Request(Message : in Message_Record)
+   procedure Handle_Move_Request(Message : in Message_Record)
      with Pre => Sample_Module.API.Is_A_Request(Message)
    is
-      Status : Message_Status_Type;
+      Status  : Message_Status_Type;
+      Voltage : Float;
+      Move_Reply : Message_Record;
    begin
-      Sample_Module.API.A_Request_Decode(Message, Status);
-      -- Act on the request message.
+      Motors.API.Move_Request_Decode
+        (Message => Message_Record,
+         Voltage => Voltage,
+         Decode_Status => Status);
+      -- moves the drone
+      Move_Reply := Motors.API.Move_Reply_Encode
+        (Receiver_Address => Message.Sender_Address,
+         Request_ID       => 1,
+         Success          => True,
+         Priority         => System.Priority);
+      Route_Message (Message => Move_Reply);
    end Handle_A_Request;
 
    -----------------------------------
@@ -51,16 +45,13 @@ package body motors.Messages is
    -- This procedure processes exactly one message at a time.
    procedure Process(Message : in Message_Record) is
    begin
-      if Sample_Module.API.Is_A_Request(Message) then
-         Handle_A_Request(Message);
+      if Motors.API.Is_Move_Request(Message => Message_Record) then
+         Handle_Move_Request(Message);
       else
          CubedOS.Log_Server.API.Log_Message(Name_Resolver.Motors,
                                             CubedOS.Log_Server.API.Error,
                                             "An unknown message type has been received!");
       end if;
-      -- When this procedure returns the message loop will immediately try to receive the next
-      -- message. Note that all CubedOS send operations are non-blocking so sending an outgoing
-      -- message will not delay execution.
    end Process;
 
    ---------------
@@ -70,7 +61,6 @@ package body motors.Messages is
    task body Message_Loop is
       Incoming_Message : Message_Manager.Message_Record;
    begin
-      Initialize;
       loop
          Message_Manager.Fetch_Message(Name_Resolver.Motors.Module_ID, Incoming_Message);
          Process(Incoming_Message);
